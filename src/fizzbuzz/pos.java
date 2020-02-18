55bd6809f136d03e19d04f1a0b2b148ac4390f2b
Petter Tobias Madsen <Petter.Madsen@student.uib.no>
2020-02-18 17:14:15 +0100

    added pos to fizzbuzz

---
+++ src/fizzbuzz/pos.java
@@ -0,0 +1,19 @@
+package fizzbuzz;
+
+public class pos {
+    private int x;
+    private int y;
+
+    public pos(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+}
---
+++ src/fizzbuzz/test.java
@@ -0,0 +1,13 @@
+package fizzbuzz;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class test {
+    @Test
+    public void testx(){
+        pos getx = new pos(3,2);
+        assertEquals(3,getx.getX());
+    }
+}
